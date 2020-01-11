36dfe96a69f25bf1bdc0a6bbbfd26a455daa1fed
lijiayoun <ily.a@foxmail.com>
2020-01-11 18:12:50 +0800

    个人管理页面

--- miniprogram/pages/home/index4.js
+++ miniprogram/pages/home/index4.js
@@ -1,66 +1,99 @@
-// pages/index/index4.js
+// 函数库
+// pages/setUp/setUp.js
 Page({
 
   /**
-   * Page initial data
+   * 页面的初始数据
    */
   data: {
-
+  
   },
 
   /**
-   * Lifecycle function--Called when page load
+   * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+  
   },
 
   /**
-   * Lifecycle function--Called when page is initially rendered
+   * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
-   * Lifecycle function--Called when page show
+   * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
-   * Lifecycle function--Called when page hide
+   * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
-   * Lifecycle function--Called when page unload
+   * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
-   * Page event handler function--Called when user drop down
+   * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
-   * Called when page reach bottom
+   * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
-   * Called when user click on the top right corner to share
+   * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
-  }
+  
+  },
+  getStorag:function(){ // 获取本地存储信息
+          var data = wx.getStorageSync("login");
+          return {
+                  sdk: data.sdk,        
+                  uid: data.uid
+          }
+  },
+  // quitFn:function(){    // 退出登陆
+  //     Utils.removeStorage("Reset"); 
+  //         var res =  this.getStorag();
+  //         Utils.requestFn({
+  //                 url: '/index.php/loginOut?server=1', 
+  //                 method:"POST",
+  //                 data: {
+  //                         sdk: res.sdk,
+  //                         uid: res.uid
+  //                 },
+  //                 success: function (res) {
+  //                         if (res.data.status){
+  //                                 wx.reLaunch({
+  //                                     url: '/pages/login/login'
+  //                                 })
+  //                                 Utils.removeStorage("login");
+                                  
+  //                         }else{
+  //                             Utils.reLaunch(res.data.message,"/pages/login/login");
+  //                             Utils.removeStorage("login");
+  //                         }
+  //                         return false;
+  //                 }
+  //         })
+  // }
 })
